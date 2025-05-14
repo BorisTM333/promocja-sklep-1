d4add41045598acf52c123c847b284b0a6c2b00a
BorisTM333 <kamila25101997@gmail.com>
2025-05-14 13:04:06 +0200

    DRUGA CZESC

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        for (int dzien = 1; dzien <= 31; dzien++) {
+        System.out.print("Podaj dzień początkowy: ");
+        int dzienPoczatkowy = sc.nextInt();
+
+        System.out.print("Podaj dzień końcowy: ");
+        int dzienKoncowy = sc.nextInt();
+
+        for (int dzien = dzienPoczatkowy; dzien <= dzienKoncowy; dzien++) {
 
             System.out.print("Dzień " + dzien + ": ");
 
             if (dzien % 3 == 0 && dzien % 5 == 0) {
-                System.out.println("Dostajesz darmową dostawe + zniżkę 10%");
-            }
-            else if(dzien % 5 == 0) {
-                System.out.println("Dostajesz zniżkę 10%");
-            }
-            else if(dzien % 3 == 0){
-                System.out.println("Dostajesz kupon na darmową dostawę");
-            }else{
+                System.out.println("Dostajesz darmowa dostawe + znizka 10%");
+            } else if (dzien % 5 == 0) {
+                System.out.println("Znizka 10%");
+            } else if (dzien % 3 == 0) {
+                System.out.println("Dostajesz kupon na darmowa dostawe");
+            } else {
                 System.out.println("Brak promocji");
             }
         }
+
+        sc.close();
     }
 }
